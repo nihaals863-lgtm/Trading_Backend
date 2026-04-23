15417f59a0a5f5419c6e566ae50f257d10979b78
akash <akashpathak2299@gmail.com>
2026-04-23 19:18:07 +0530

    pull

--- src/server.js
+++ src/server.js
@@ -19,6 +19,7 @@ const server = http.createServer(app);
 
 const ALLOWED_ORIGINS = [
     'http://localhost:5173',
+    'http://localhost:5174',
     'http://localhost:8081',
     'https://traderss.kiaantechnology.com',
     process.env.FRONTEND_URL
